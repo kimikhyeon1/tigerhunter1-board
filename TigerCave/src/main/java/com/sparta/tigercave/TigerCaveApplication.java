71f9c7d74acf9fbfce42d53d969517db89a262e8
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2022-12-30 17:12:50 +0900

    Feat: 프로젝트 기본 구성 생성

---
+++ TigerCave/src/main/java/com/sparta/tigercave/TigerCaveApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.tigercave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerCaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TigerCaveApplication.class, args);
+    }
+
+}
---
+++ TigerCave/src/test/java/com/sparta/tigercave/TigerCaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.tigercave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerCaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
