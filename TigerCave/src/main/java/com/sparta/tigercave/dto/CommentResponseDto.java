5d3c78050762fe9a527f761a88351e8b605678ae
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-01-03 18:04:20 +0900

    Fix :  CommentResponseDto 수정
    Timestamped String 수정 PostResponseDto 오타 수정 CommentResponseDto Timestamped 수정

--- TigerCave/src/main/java/com/sparta/tigercave/dto/CommentResponseDto.java
+++ TigerCave/src/main/java/com/sparta/tigercave/dto/CommentResponseDto.java
@@ -1,7 +1,10 @@
 package com.sparta.tigercave.dto;
 
 import com.sparta.tigercave.entity.Comment;
+import com.sparta.tigercave.entity.Timestamped;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class CommentResponseDto {
@@ -9,7 +12,12 @@ public class CommentResponseDto {
     private Long id;
     private String comment;
     private Long comment_likes;
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
+
     public CommentResponseDto(Comment comment) {
+        this.createAt = comment.getCreateAt(); // post.getCreatedAt(); -> post.getCreateAt(); 으로 수정 했습니다.
+        this.modifiedAt = comment.getModifiedAt();
         this.id = comment.getId();
         this.comment = comment.getComment();
         this.comment_likes = comment.getComment_likes();
