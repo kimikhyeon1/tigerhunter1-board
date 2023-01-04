52531914f85fc86f228f810dee097e9b1d588fb0
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-01-04 09:59:08 +0900

    Feat: addlike 주석 추가

--- TigerCave/src/main/java/com/sparta/tigercave/service/PostLikeService.java
+++ TigerCave/src/main/java/com/sparta/tigercave/service/PostLikeService.java
@@ -8,7 +8,6 @@ import com.sparta.tigercave.repository.PostRepository;
 import com.sparta.tigercave.repository.UsersRepository;
 import com.sparta.tigercave.security.UserDetailImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
@@ -24,13 +23,18 @@ public class PostLikeService {
 
     @Transactional
     public Long addLike(Long postId, UserDetailImpl userDetails) {
+        // 게시글 존재 여부 체크
         Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("없는 게시물"));
+        // userDetails에서 userId 체크
         User user = userRepository.findById(userDetails.getUserId()).get();
+        // 게시글에 유저좋아요 기록이 있는지 확인
         List<PostLike> postLikes  = postLikeRepository.findByUserAndPost(user,post);
+        // 기록이 없다면 저장
         if (postLikes.isEmpty()){
             postLikeRepository.save(new PostLike(post,user));
             return postId;
         }
+        // 기록이 있다면 삭제
         postLikeRepository.delete(postLikes.get(0));
         return postId;
     }
