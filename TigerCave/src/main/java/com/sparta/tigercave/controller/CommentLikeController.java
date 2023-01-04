94cf2555dba2a22a0926ff3ff34eb05602f97abe
Rubi <quipu1@naver.com>
2023-01-04 19:05:08 +0900

    Fix: 좋아요 등록, 취소 로직 분리

--- TigerCave/src/main/java/com/sparta/tigercave/controller/CommentLikeController.java
+++ TigerCave/src/main/java/com/sparta/tigercave/controller/CommentLikeController.java
@@ -1,5 +1,6 @@
 package com.sparta.tigercave.controller;
 
+import com.sparta.tigercave.exception.CustomException;
 import com.sparta.tigercave.security.UserDetailImpl;
 import com.sparta.tigercave.service.CommentLikeService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import static com.sparta.tigercave.exception.ErrorCode.*;
+
 @RestController
 @RequestMapping("/api/post/comment")
 @RequiredArgsConstructor
@@ -15,18 +18,30 @@ public class CommentLikeController {
 
     private final CommentLikeService commentLikeService;
 
-    // 좋아요 추가
+    // 좋아요 등록
     @PostMapping("/{comment_id}/like")
-    public ResponseEntity addOrDeleteLike(@PathVariable Long comment_id, @AuthenticationPrincipal UserDetailImpl userDetail) {
-        Boolean likeOpt = commentLikeService.addOrDeleteLike(comment_id, userDetail.getUserId());
+    public ResponseEntity addLike(@PathVariable Long comment_id, @AuthenticationPrincipal UserDetailImpl userDetail) {
+        Boolean likeOpt = commentLikeService.addLike(comment_id, userDetail.getUserId());
 
-        // 좋아요 등록
+        // 좋아요 등록, 이미 좋아요를 한 사용자의 경우 Exception
         if (likeOpt) {
             return new ResponseEntity<>("좋아요!", HttpStatus.OK);
+        }else {
+            throw new CustomException(ALREADY_LIKE_USER);
         }
+    }
 
-        // 좋아요 삭제
-        return new ResponseEntity<>("좋아요 취소", HttpStatus.OK);
+    //좋아요 취소
+    @DeleteMapping("/{comment_id}/like")
+    public ResponseEntity deleteLike(@PathVariable Long comment_id, @AuthenticationPrincipal UserDetailImpl userDetail) {
+        Boolean likeOpt = commentLikeService.deleteLike(comment_id, userDetail.getUserId());
+
+        // 좋아요 취소, 아직 좋아요를 안 한 사용자의 경우 Exception
+        if (likeOpt) {
+            return new ResponseEntity<>("좋아요 취소", HttpStatus.OK);
+        } else {
+            throw new CustomException(NOT_YET_LIKE_USER);
+        }
     }
 
 }
