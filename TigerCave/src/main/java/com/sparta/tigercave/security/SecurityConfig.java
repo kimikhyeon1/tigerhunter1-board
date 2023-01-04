bd501d88a4b5153c5767e50958ca4f0541b40171
seinHan <seinhan151@gmail.com>
2023-01-04 17:34:21 +0900

    Fix : SecurityConfig.java에서 회원가입, 로그인 url-> jwt필터 타지않도록 수정함.

--- TigerCave/src/main/java/com/sparta/tigercave/security/SecurityConfig.java
+++ TigerCave/src/main/java/com/sparta/tigercave/security/SecurityConfig.java
@@ -41,17 +41,14 @@ public class SecurityConfig {
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);    //세션을 사용하지 않는다고 설정한다.
 
-        http.authorizeRequests()                                    //요청에 대한 사용권한 체크
-                .antMatchers("/api/**").hasRole("ADMIN")
-                .antMatchers("/api/**").hasRole("USER") //.antMatchers() -> 해당 URL로 요청 시 permitAll()-> 접근을 허용해준다.
-                .anyRequest().permitAll()
+        http.authorizeRequests()
+                .antMatchers("/api/user/signup").permitAll()   //해당 url은 필터를 거치지 않도록 설정
+                .antMatchers("/api/user/login").permitAll()   //해당 url은 필터를 거치지 않도록 설정
+                .antMatchers("/api/**").hasRole("ADMIN")      //ADMIN, USER권한은 JWT필터를 거치도록 함
+                .antMatchers("/api/**").hasRole("USER")
+                .anyRequest().authenticated()
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
-                //왼쪽은 커스텀한 필터링이 들어간다. 오른쪽에 등록한 필터전에 커스텀필터링 실행
 
-        //궁금한 점
-        //그렇다면 토큰이 발행되기 전인 회원가입, 로그인, 게시판 전체 조회 부분을 필터 타게 하지않고
-        //나머지 글 작성, 글 수정, 삭제 부분을 허용해야하는거 아닌가?
-        //이 부분도 각자 설계하기 나름인 부분인지..
 
         http.formLogin().loginPage("/api/user/login?error")
                 .failureUrl("/")
