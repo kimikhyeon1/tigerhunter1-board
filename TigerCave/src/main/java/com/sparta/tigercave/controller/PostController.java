eab9274753478bcafdc5abefdbbef026c2296f0a
SeokJin <tae993136@gmail.com>
2023-01-04 18:17:06 +0900

    Fix: post url수정

--- TigerCave/src/main/java/com/sparta/tigercave/controller/PostController.java
+++ TigerCave/src/main/java/com/sparta/tigercave/controller/PostController.java
@@ -25,7 +25,7 @@ public class PostController {
 
 
     //게시글 목록
-    @GetMapping("/api/post")
+    @GetMapping("/api/posts")
     public List<PostResponseDto> getPostList(){
         return postService.getPostList();
     }
@@ -37,9 +37,9 @@ public class PostController {
     }
 
     // 게시글 수정
-    @PutMapping("/api/post/{id}")
-    public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto, @AuthenticationPrincipal UserDetails userDetails) {
-        return postService.updatePost(id, postRequestDto, userDetails);
+    @PutMapping("/api/post/{post_id}")
+    public PostResponseDto updatePost(@PathVariable Long post_id, @RequestBody PostRequestDto postRequestDto, @AuthenticationPrincipal UserDetails userDetails) {
+        return postService.updatePost(post_id, postRequestDto, userDetails);
     }
     // 게시글 삭제
     @DeleteMapping("/api/post/{post_id}")
