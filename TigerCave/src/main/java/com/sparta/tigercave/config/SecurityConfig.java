6f41022b997fc14bef4015adcb666e37539b5dd8
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-01-05 15:52:57 +0900

    Fix : hasRole 수정

--- TigerCave/src/main/java/com/sparta/tigercave/config/SecurityConfig.java
+++ TigerCave/src/main/java/com/sparta/tigercave/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sparta.tigercave.config;
 
+import com.sparta.tigercave.entity.UserRoleEnum;
 import com.sparta.tigercave.jwt.JwtAuthFilter;
 import com.sparta.tigercave.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
@@ -43,17 +44,18 @@ public class SecurityConfig {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);    //세션을 사용하지 않는다고 설정한다.
 
         http.authorizeRequests()
-                .antMatchers("/api/user/signup").permitAll()   //해당 url은 필터를 거치지 않도록 설정
+                .antMatchers("/api/user/signup").permitAll()   //해당 url은 필터를 거치지 않도록 설정 수정 permitAll은
                 .antMatchers("/api/user/login").permitAll()   //해당 url은 필터를 거치지 않도록 설정
-                .antMatchers("/api/**").hasRole("ADMIN")      //ADMIN, USER권한은 JWT필터를 거치도록 함
-                .antMatchers("/api/**").hasRole("USER")
+                .antMatchers("/api/**").hasAnyRole(UserRoleEnum.ADMIN.name(), UserRoleEnum.USER.name())      //ADMIN, USER권한은 JWT필터를 거치도록 함
+//                .antMatchers("/api/**").hasRole("USER")
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
 
-        http.formLogin().loginPage("/api/user/login?error")
-                .failureUrl("/")
-                .permitAll();
+        http.formLogin().loginProcessingUrl("/api/user/");
+//                loginPage("/api/user/login?error") // 이상해씨 login-page 로그인 메소드 요청 구분해서
+//                .failureUrl("/")
+//                .permitAll();
         return http.build();
     }
 }
